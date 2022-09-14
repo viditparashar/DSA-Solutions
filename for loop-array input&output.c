b31e4f63cef424ea007d9e4b4e922bb0db703996
Vidit Parashar <88538261+viditparashar@users.noreply.github.com>
2022-09-14 12:27:47 +0530

    Add files via upload

---
+++ for loop-array input&output.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i,j;
+    int Kim[6];
+    printf("Write 4 elements of Ele");
+    for (int i = 0; i<=7; i++){
+        scanf("%d", &Kim[i]); 
+        }
+        for(j =0; j<7; j++){
+            printf( "%d\n" , Kim[j]);
+            }
+        
+    
+    
+
+    return 0;
+}
