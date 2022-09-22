176a234e347aa343539c9de2b0e63ff08ae9e1f4
Vidit Parashar <88538261+viditparashar@users.noreply.github.com>
2022-09-22 19:51:17 +0530

    Update to do list codechef.c

--- to do list codechef.c
+++ to do list codechef.c
@@ -8,16 +8,17 @@ int main(void) {
 	scanf("%d",&t);
 	
 	while(t--){
+		//declare values in while loop so that values of count doesn't set to 0 again.
 		int n,i,count=0,a[1000];
-	    scanf("%d",&n);
-        for(i=0;i<n;i++){
-            
-            scanf("%d",&a[i]);
-            if((a[i])>=1000){
-                count = count + 1;
+		scanf("%d",&n);
+		for(i=0;i<n;i++){
+			
+			scanf("%d",&a[i]);
+			if((a[i])>=1000){
+				count = count + 1;
                 
-            }
-	    }
+                        }
+	        }
 		printf("%d\n",count);
            
 	}
